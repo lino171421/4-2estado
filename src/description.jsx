1dabd9007ff236b7b4911b6579c5f96f60c11365
lino171421 <viziator.1714@gmail.com>
2023-05-09 04:24:55 -0400

    Initial commit

---
+++ src/description.jsx
@@ -0,0 +1,14 @@
+function Profile()  {
+    return(
+        <img src="https://static.eldiario.es/clip/71d118ff-5ef2-449c-be8a-6c321304fa70_16-9-aspect-ratio_default_0.jpg" alt="celu" />
+    );
+}
+
+export default function Gallery()   {
+    return (
+        <section>
+            <p>hola</p>
+            <Profile/>
+        </section>
+    )
+}
